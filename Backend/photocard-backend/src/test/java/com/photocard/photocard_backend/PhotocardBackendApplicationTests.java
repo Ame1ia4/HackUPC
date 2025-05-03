59424fe0b37a50db98eb1c444aec4e84e58a0832
Best <hollyjanebest@gmail.com>
2025-05-03 13:54:18 +0200

    photocard backend stuff

---
+++ Backend/photocard-backend/src/test/java/com/photocard/photocard_backend/PhotocardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photocard.photocard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotocardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
